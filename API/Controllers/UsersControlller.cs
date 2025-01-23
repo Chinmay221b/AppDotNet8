e28030609a854e5d6cdef33fbc90256d0cb36e0b
Chinmay Kulkarni <chinmaykulkarni829@gmail.com>
2025-01-23 16:51:56 +0530

    changes for Async code

--- API/Controllers/UsersControlller.cs
+++ API/Controllers/UsersControlller.cs
@@ -1,6 +1,7 @@
 using API.Data;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
@@ -9,17 +10,17 @@ namespace API.Controllers;
 public class UsersController(DataContext context) : ControllerBase
 {
     [HttpGet]
-    public ActionResult<IEnumerable<AppUser>> GetUsers()
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
     {
-        var users = context.Users.ToArray();
+        var users = await context.Users.ToArrayAsync();
         return users;
     }
 
-    // [HttpGet("{id:int}")]
-    // public ActionResult<AppUser> GetUsers(int id)
-    // {
-    //     var user = context.Users.Find(id);
-    //     if (user == null) return NotFound();
-    //     return user;
-    // }
+    [HttpGet("{id:int}")]
+    public async Task<ActionResult<AppUser>> GetUsers(int id)
+    {
+        var user = await context.Users.FindAsync(id);
+        if (user == null) return NotFound();
+        return user;
+    }
 }
